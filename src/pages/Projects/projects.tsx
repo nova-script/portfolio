1fe47c7ce178342579ac4ea73d24e7be2c301e65
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-09 12:33:14 -0300

    :bulb: Create Projects page

---
+++ src/pages/Projects/projects.tsx
@@ -0,0 +1,12 @@
+import bendita from "./img/bendita.png";
+
+export default [
+  {
+    title: "Bendita Digital",
+    description: "aaaaaaaaaaaaaaaaaaaaaaa",
+    url: "http://bendita.digital",
+    image: bendita,
+    hasSourceCode: true,
+    sourceCode: "https://github.com/benditadigital/website",
+  },
+];
