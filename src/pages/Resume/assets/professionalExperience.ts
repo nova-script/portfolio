3910b0803517635fcc0a843333758a760aef2208
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-12 14:27:13 -0300

    :bulb: Add Resume assets

---
+++ src/pages/Resume/assets/professionalExperience.ts
@@ -0,0 +1,50 @@
+export default [
+  {
+    title: "Full-Stack Developer @LedSHOW",
+    startDate: "(11/2021)",
+    finishDate: "(06/2022)",
+    type: "Hybrid work",
+    attribuitions: [
+      "Complete development and maintenance of an API using Django Rest Framework (Python), PostgreSQL and MongoDB.",
+      "Development of mobile application for Android using Expo and React Native.",
+      "Development of website containing login, register, user information and contact using React and Bootstrap.",
+      "Development of admin dashboard using React and Bootstrap.",
+      "Manage CI/CD with GitHub Actions on AWS EC2 and Digital Ocean, using Docker, NGINX and GUnicorn.",
+    ],
+  },
+  {
+    title: "Intern Data Engineer @DataPolicy",
+    startDate: "(06/2021)",
+    finishDate: "(11/2021)",
+    type: "Remote work",
+    attribuitions: [
+      "Development and maintenance of several Python/MongoDB/PostgreSQL webscrappers.",
+      "Creation of name matching algorithm using Levenshtein distance formula.",
+      "Developed of JCaptcha solver using OpenCV and Tesseract OCR.",
+      "Improved code review process.",
+    ],
+  },
+  ,
+  {
+    title: "Intern Full-Stack Developer @Federal Police of Brazil",
+    startDate: "(02/2021)",
+    finishDate: "(06/2021)",
+    type: "Hybrid work",
+    attribuitions: [
+      "Development and maintenance of several Python/MongoDB/PostgreSQL/Elastic webscrappers.",
+      "Responsive web development of dashboards using Django, Bootstrap and JQuery.",
+      "File processing and building (PDF, XML, CSV).",
+    ],
+  },
+  ,
+  {
+    title: "Intern Full-Stack Developer @Coding55",
+    startDate: "(06/2021)",
+    finishDate: "(11/2021)",
+    type: "Remote work",
+    attribuitions: [
+      "Development and maintenance of an online classes platform using Django and Bootstrap.",
+      "Development and maintenance of API using Django Rest Framework, for a mobile game.",
+    ],
+  },
+];
---
+++ src/pages/Resume/assets/technicalProficiences.ts
@@ -0,0 +1,40 @@
+export default [
+  {
+    title: "Programming Languages:",
+    description: "Python, JavaScript, TypeScript, C/C++.",
+  },
+  {
+    title: "Front-end:",
+    description:
+      "HTML5/CSS3, React, React Native, Bootstrap, Tailwind, MaterialUI, HeadlessUI, Relay (GRAPHQL).",
+  },
+  {
+    title: "Back-end:",
+    description:
+      "Django/Django Rest Framework (Python), Node (JavaScript), Express (TypeScript), Koa, Docker, GraphQL, NGINX.",
+  },
+  {
+    title: "Cloud:",
+    description: "AWS EC2, Digital Ocean, Heroku",
+  },
+  ,
+  {
+    title: "Databases:",
+    description: "PostgreSQL, MySQL/MariaDB, SQLite, Elasticsearch, MongoDB.",
+  },
+  ,
+  {
+    title: "Operational Systems:",
+    description: "Linux (red. Ubuntu, Mint, Fedora), Windows 10.",
+  },
+  ,
+  {
+    title: "Web Scrapping:",
+    description: "Scrapy, Splash, Selenium, BeautifulSoup, Tesseract OCR.",
+  },
+  ,
+  {
+    title: "Agile Methodologies:",
+    description: "SAFe, Scrum, Kanban.",
+  },
+];
