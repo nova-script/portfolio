67d9c567ac5863fb34670d1a97d8a5c02fee6c9b
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-12 14:28:28 -0300

    :arrow_up: Update Projects

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -6,6 +6,11 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { currentPathAtom } from "../../App";
 
+import "./styles.css";
+
+/* Icons */
+import { BiLinkExternal, BiCodeBlock } from "react-icons/bi";
+
 /* Projects */
 import projects from "./projects";
 
@@ -14,18 +19,64 @@ interface Project {
   description: string;
   image: string;
   url: string;
+  technologyStack: Array<string>;
   hasSourceCode: boolean;
   sourceCode?: string;
+  finishDate: string;
 }
 
 function renderProjects() {
+  function renderCoreStack(technologyStack: Array<string>) {
+    return technologyStack.map((technology: string) => {
+      return <span className="technology-stack-badge">{technology}</span>;
+    });
+  }
+
   return projects.map((project: Project) => {
     return (
-      <div className="project-box my-10">
-        <img className="col-span-5" width="100%" src={project.image} />
-        <div className="overlay">
-          <h1> teste </h1>
-          <p> xampson </p>
+      <div className="project-box-wrapper col-span-5">
+        <div className="project-box p-3 pb-0 h-[100%] ">
+          <div className="img-wrapper">
+            <img width="100%" src={project.image} />
+            <div className="img-overlay"></div>
+          </div>
+
+          <div className="overlay p-5 flex flex-col">
+            <div>
+              <a className="mb-10" href={project.url} target="_blank">
+                <h3 className="flex items-center gap-2">
+                  {project.title}
+                  <BiLinkExternal size={20} className="mt-1" color="gray" />
+                </h3>
+              </a>
+              <div>
+                {project.hasSourceCode ? (
+                  <a
+                    className="source-code"
+                    href={project.sourceCode}
+                    target="_blank"
+                  >
+                    <small className="flex gap-3 text-[15px] items-center">
+                      READ SOURCE CODE <BiCodeBlock color="gray" size={15} />
+                    </small>
+                  </a>
+                ) : (
+                  <div className="mt-6"></div>
+                )}
+              </div>
+            </div>
+
+            <p className="mt-5 text-justify">{project.description}</p>
+            <hr className="mt-3" />
+            <div className="my-3">
+              <h4 className="mb-2">Core stack:</h4>
+              <div className="">{renderCoreStack(project.technologyStack)}</div>
+            </div>
+
+            <div className="text-right mt-5">
+              <small className="mt-5 text-[16px]">{project.finishDate}</small>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -39,6 +90,10 @@ export default function Projects() {
     setCurrentPath(window.location.pathname);
   }, []);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <motion.section
       initial={{ opacity: 0, x: 100 }}
@@ -48,7 +103,7 @@ export default function Projects() {
       className="projects pl-10 pt-[90px]"
     >
       <div className="container mx-auto max-w-[1024px]">
-        <div>
+        <div className="pb-10">
           <h1 className="leading-1">Websites Everywhere!</h1>
           <h2 className="leading-none">Some examples of my work! :)</h2>
           <hr className="mt-5" />
@@ -61,7 +116,7 @@ export default function Projects() {
             <br />
           </p>
         </div>
-        <div className="mt-10">{renderProjects()}</div>
+        <div className="grid grid-cols-10 gap-5 mt-10">{renderProjects()}</div>
       </div>
     </motion.section>
   );
--- src/pages/Projects/projects.tsx
+++ src/pages/Projects/projects.tsx
@@ -1,12 +1,84 @@
 import bendita from "./img/bendita.png";
+import hollow from "./img/hollow.png";
+import panyart from "./img/panyart.png";
+import iplocation from "./img/iplocation.png";
+import duofinder from "./img/duofinder.jpg";
 
 export default [
   {
     title: "Bendita Digital",
-    description: "aaaaaaaaaaaaaaaaaaaaaaa",
+    description:
+      "Modern brazilian digital agency landing page, performant and accessible.",
     url: "http://bendita.digital",
     image: bendita,
+    technologyStack: [
+      "React",
+      "Node",
+      "Tailwind",
+      "HeadlessUI",
+      "Recoil",
+      "TypeScript",
+    ],
     hasSourceCode: true,
     sourceCode: "https://github.com/benditadigital/website",
+    finishDate: "12/2022",
+  },
+  {
+    title: "Hollow",
+    description:
+      "Online video lessons platform for college students in Portugal.",
+    url: "http://hollow.pt",
+    image: hollow,
+    technologyStack: ["Django", "Bootstrap", "Python", "Vimeo", "PostgreSQL"],
+    hasSourceCode: false,
+    finishDate: "01/2020",
+  },
+  {
+    title: "PanYart",
+    description:
+      "Online store for a digital artist. His clients can buy art stuff using his own digital coin.",
+    url: "https://panyart.studio/",
+    image: panyart,
+    technologyStack: [
+      "React",
+      "Bootstrap",
+      "Express",
+      "JavaScript",
+      "Node",
+      "PostgreSQL",
+    ],
+    hasSourceCode: true,
+    sourceCode:
+      "https://github.com/nova-script?tab=repositories&q=panyart&type=&language=&sort=",
+    finishDate: "10/2022",
+  },
+  {
+    title: "IP Location",
+    description:
+      "Enter someone's IP and locate them, also get some additional information.",
+    url: "https://nova-script.github.io/IP-location/",
+    image: iplocation,
+    technologyStack: ["React", "Node", "Typescript", "Tailwind"],
+    hasSourceCode: true,
+    sourceCode: "https://github.com/nova-script/IP-location/",
+    finishDate: "10/2022",
+  },
+  {
+    title: "Duo Finder",
+    description:
+      "Find someone to play the most popular online multiplayer games with you.",
+    image: duofinder,
+    technologyStack: [
+      "React",
+      "Node",
+      "RadixUI",
+      "Typescript",
+      "Tailwind",
+      "Expo",
+      "React Native",
+    ],
+    hasSourceCode: true,
+    sourceCode: "https://github.com/nova-script/duo-finder",
+    finishDate: "09/2022",
   },
 ];
