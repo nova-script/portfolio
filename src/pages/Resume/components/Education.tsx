ed9376f0c8193faa7f3d74780225a07a08f6e46a
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-12 14:26:23 -0300

    :bulb: Add Education

---
+++ src/pages/Resume/components/Education.tsx
@@ -0,0 +1,30 @@
+export default function Education() {
+  return (
+    <div className="mb-10">
+      <div>
+        <h3 className="font-medium">EDUCATION:</h3>
+        <hr className="my-3 w-[100%]" />
+        <div>
+          <p>
+            <strong>Software Engineering @University of Brasília (UnB)</strong>
+          </p>
+          <small>(2018 - 2024)</small>
+          <p className=""> </p>
+          <p>Ongoing graduation.</p>
+        </div>
+
+        <div className="mt-5">
+          <p>
+            <strong>
+              Advanced English @Interscholastic Center for Languages of
+              Taguatinga (CILT)
+            </strong>
+          </p>
+          <small>(2013 - 2018)</small>
+          <p className=""> </p>
+          <p>Fluency acquired.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
